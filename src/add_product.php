de32359b12c3e216d22063db29bd66adf7768f50
manivourne <manivournthon@gmail.com>
2022-07-07 14:27:31 +0700

    add

--- src/add_product.php
+++ src/add_product.php
@@ -4,19 +4,24 @@ require '..\vendor\autoload.php';
 
 $pdo = new PDO('mysql:dbname=learningphp', 'root', '');
 $fluent = new Envms\FluentPDO\Query($pdo);
+if (isset($_POST)) {
+    $product_name = $_POST['name'];
+    $amount = (int)$_POST['amount'];
+    $price = (int)$_POST['price'];
+    $values = [
+        'id' => '',
+        'name' => $product_name,
+        'user_id' => 1,
+        'amount' => $amount,
+        'price' => $price,
+    ];
 
-$values = [
-    'id' => '',
-    'name' => 'content1',
-    'user_id' => 1,
-    'amount' => 10,
-    'price' => 20,
-];
-
-$query = $fluent
-    ->insertInto('products')
-    ->values($values)
-    ->execute();
+    $query = $fluent
+        ->insertInto('products')
+        ->values($values)
+        ->execute();
+    echo "product added successfully";
+}
 
 // $query = $fluent->insertInto('products', $values)->execute();
 // 
